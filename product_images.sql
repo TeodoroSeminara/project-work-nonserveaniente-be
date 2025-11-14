ee8e6554f8c724b1f35145a7cfc47b6ba7820023
MassimilianoTusini <massimilianotusini110@gmail.com>
2025-11-15 00:53:33 +0100

    Added some photos and Sql file for images

---
+++ product_images.sql
@@ -0,0 +1,40 @@
+INSERT INTO product_images (id, product_id, image_url) VALUES
+ -- 1) Tazza senza manico
+(1, 1, 'handleless_mug_1.png'),
+(2, 1, 'handleless_mug_2.png'),
+
+ -- 2) Cucchiaio che si scioglie
+(3, 2, 'melting_spoon_1.png'),
+(4, 2, 'melting_spoon_2.png'),
+
+ -- 3) Ciabatte con GPS integrato
+(5, 3, 'gps_slippers_1.png'),
+(6, 3, 'gps_slippers_2.png'),
+
+-- 4) Ombrello per bicicletta
+(7, 4, 'bicycle_umbrella_1.png'),
+(8, 4, 'bicycle_umbrella_2.png'),
+
+-- 5) Cappello con ventilatore integrato
+(9, 5, 'fan_hat_1.png'),
+(10, 5, 'fan_hat_2.png'),
+
+-- 6) Portachiavi a labirinto
+(11, 6, 'maze_keychain_1.png'),
+(12, 6, 'maze_keychain_2.png'),
+
+-- 7) Calzini antiscivolo irriverenti
+(13, 7, 'funny_grip_socks_1.png'),
+(14, 7, 'funny_grip_socks_2.png'),
+
+-- 8) Tappetino da bagno “spiaggia”
+(15, 8, 'beach_bath_mat_1.png'),
+(16, 8, 'beach_bath_mat_2.png'),
+
+-- 9) Occhiali per dormire (mascherina)
+(17, 9, 'sleep_mask_1.png'),
+(18, 9, 'sleep_mask_2.png'),
+
+-- 10) Zaino con tasca segreta
+(19, 10, 'secret_pocket_backpack_1.png'),
+(20, 10, 'secret_pocket_backpack_2.png');
