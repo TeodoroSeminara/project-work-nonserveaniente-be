049060532f84035ac0823d37cc40fb52ffd1a69e
Teodoro <seminara.teodoro93@gmail.com>
2025-11-16 19:38:17 +0100

    path correction

--- middlewares/ImagePath.js
+++ middlewares/ImagePath.js
@@ -1,6 +1,7 @@
 function setImagePath(req, res, next) {
-    req.imagePath = `${req.protocol}://${req.get('host')}/img/`;
+    req.imagePath = `${req.protocol}://${req.get('host')}/images/`;
     next()
 }
+
 
 module.exports = setImagePath;
